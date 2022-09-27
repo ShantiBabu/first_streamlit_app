5068506f98c73a5cace83b6dd8ff1ae907b984e4
SantiB <114508504+ShantiBabu@users.noreply.github.com>
2022-09-27 13:09:55 -0400

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 
-streamit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
